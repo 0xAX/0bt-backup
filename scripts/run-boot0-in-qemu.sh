e0d43bc25e762f726377d221f095cc4bb140ffa4
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-06-11 23:44:15 +0600

    scripts: add debug option to run-boot0-in-qemu script
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- scripts/run-boot0-in-qemu.sh
+++ scripts/run-boot0-in-qemu.sh
@@ -1,4 +1,20 @@
 #!/bin/bash
 
-make all && \
-qemu-system-x86_64 ./src/x86_64/boot0_x86_64.bin
+if [[ "$#" -eq 0 ]];
+then
+    make all && qemu-system-x86_64 ./src/x86_64/boot0_x86_64.bin
+    exit 0
+fi
+
+if [[ "$1" = "debug" ]];
+then
+   make all && qemu-system-x86_64 -s -S ./src/x86_64/boot0_x86_64.bin
+   exit 0
+fi
+
+echo "run-boot0-in-qemu.sh - runs plain boot0 in qemu"
+echo "Usage:"
+echo "  * debug - run qemu and open a gdbserver on TCP port 1234."
+echo "  * no arguments - run the boot0_x86_64.bin in qemu."
+
+exit 0
