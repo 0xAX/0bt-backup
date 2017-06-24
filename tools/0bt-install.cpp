a5a949704f85433351ca44a1247c6a1d48306822
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-06-25 00:31:32 +0600

    0bt-install: open a disk and a MBR binary for writing/reading
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- tools/0bt-install.cpp
+++ tools/0bt-install.cpp
@@ -7,22 +7,72 @@
  * given storage.
  */
 
+#include <fstream>
 #include <iostream>
 
 using namespace std;
 
-static const char *default_disk_image = "disk.img";
+#define MBR_SIZE 446
 
-[[noreturn]]
-static void usage() {
+static const string default_disk_image = "disk.img";
+static const string default_mbr_path = "/boot/0bt/boot0_x86_64.bin";
+
+[[noreturn]] static void usage()
+{
 	cout << "0bt-install Ver - " << VERSION << endl;
-	cout << "Usage: 0bt-install [diskname]" << endl;
+	cout << "Usage: 0bt-install [diskname] [path to mbr]" << endl;
+	cout << endl;
+	cout << "  -h/--help - print this message" << endl;
 	exit(0);
 }
 
-int main(int argc, char *argv[]) {
+static void write_bootloader(const string *disk, const string *mbr)
+{
+	fstream disk_fstream;
+	fstream mbr_fstream;
 
-	if (argc == 1)
+	disk_fstream.open(*disk, fstream::in | fstream::binary);
+
+	if (!disk_fstream)
+	{
+		perror("error while opening a device");
+		exit(1);
+	}
+
+	mbr_fstream.open(*mbr, fstream::in | fstream::binary);
+
+	if (!mbr)
+	{
+		perror("error while opening a MBR file");
+		disk_fstream.close();
+		exit(1);
+	}
+
+	/*
+	 * TODO write mbr here
+	 */
+
+	mbr_fstream.close();
+	disk_fstream.close();
+}
+
+int main(int argc, char *argv[])
+{
+	const string *disk = nullptr;
+	const string *mbr = &default_mbr_path;
+
+	if (argc == 2 && ((string(argv[1]).compare("-h") == 0) ||
+			  (string(argv[1]).compare("--help") == 0)))
 		usage();
+
+	if (argc == 2)
+		disk = new string(argv[1]);
+	else
+		disk = new string(default_disk_image);
+
+	write_bootloader(disk, mbr);
+
+	delete disk;
+
 	return 0;
 }
