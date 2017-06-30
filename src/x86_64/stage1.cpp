879239c3d9f962be0c2b0c80f26f4a9081e878cb
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-07-01 00:20:08 +0600

    stage1: add headers to *.cpp files
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- src/x86_64/stage1.cpp
+++ src/x86_64/stage1.cpp
@@ -1,3 +1,11 @@
+/*
+ * -*- Mode: c++; indent-tabs-mode: t -*-
+ *
+ * This file is part of 0bt. For more info see COPYING.
+ *
+ * Entry point of stage 1.
+ */
+
 #include "stage1.hpp"
 
 const char *greetings = "Hello from 0bt!";
--- src/x86_64/tty.cpp
+++ src/x86_64/tty.cpp
@@ -1,3 +1,13 @@
+/*
+ * -*- Mode: c++; indent-tabs-mode: t -*-
+ *
+ * This file is part of 0bt. For more info see COPYING.
+ *
+ * This source code file provides API for input/output
+ * via different sources/destinations like BIOS, serial
+ * I/O and etc.
+ */
+
 #include "stage1.hpp"
 
 void bios_print_string(const char *str)
