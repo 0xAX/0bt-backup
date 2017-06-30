170a9b501d973a0b92c1ab68d644489da5e44a97
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-07-01 00:17:37 +0600

    general: format C++ code
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- src/x86_64/stage1.cpp
+++ src/x86_64/stage1.cpp
@@ -1,10 +1,11 @@
 #include "stage1.hpp"
 
-const char* greetings = "Hello from 0bt!";
+const char *greetings = "Hello from 0bt!";
 
 extern "C" {
-	void stage1_main() {
-		clear_screen();
-		bios_print_string(greetings);
-	}
+void stage1_main()
+{
+	clear_screen();
+	bios_print_string(greetings);
+}
 }
