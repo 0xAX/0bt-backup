c30edee96e307116853503898f98cadb6425d8c2
Alexander Kuleshov <kuleshovmail@gmail.com>
2017-07-01 00:08:11 +0600

    scripts: import apply-clang-format.sh script
    add more targets
    
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- scripts/apply-clang-format.sh
+++ scripts/apply-clang-format.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-basedir=$(pwd)
-toolsdir=$basedir/tools
-find $toolsdir -iname *.hpp -o -iname *.cpp | xargs clang-format -i
+# where the script is runned from
+base_dir=$(pwd)
+# basename of the current directory where the script is runned from
+bootloader_root_dir=$(basename $base_dir)
+
+#
+# we should be runned from 0bt root dir
+#
+if [[ $"bootloader_root_dir" != "0bt" ]];
+then
+    echo -e "\033[31mError: \e[0mapply-clang-format.sh should be runned from root 0bt directory."
+    exit 1
+fi
+
+#
+# directories with c++ code
+#
+tools_dir=$base_dir/tools
+x86_64_src=$base_dir/src/x86_64
+
+#
+# apply clang script for this directories
+#
+find $tool_sdir -iname *.hpp -o -iname *.cpp | xargs clang-format -i
+find $x86_64_src -iname *.hpp -o -iname *.cpp | xargs clang-format -i
+
 exit 0
